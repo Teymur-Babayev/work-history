425f83cb6eaf899d151a8d9dec5f1756490ccbea
Easyprogramador <teymurdream@gmail.com>
2024-04-28 01:52:12 +1000

    third

---
+++ mobile-responsive design/assets/js/index.js
@@ -0,0 +1,33 @@
+
+
+//scroll reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+   origin: 'left',
+   duration: 1000,
+   distance: '50%',
+   delay: 300
+});
+
+sr.reveal('.animate-right', {
+   origin: 'right',
+   duration: 1000,
+   distance: '50%',
+   delay: 600
+});
+
+sr.reveal('.animate-top', {
+   origin: 'top',
+   duration: 1000,
+   distance: '50%',
+   delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+   origin: 'bottom',
+   duration: 1000,
+   distance: '50%',
+   delay: 600
+});
